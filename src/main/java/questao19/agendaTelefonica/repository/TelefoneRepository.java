87947ca25727f71d64120fafadece43894710597
lislenytorquato <lislenytorquato19@gmail.com>
2025-08-01 10:52:51 -0300

    refactor: Deleting the Favoritos classes: model and repository; and for Telefone, only the service, repository and service test

--- src/main/java/questao19/agendaTelefonica/repository/TelefoneRepository.java
+++
@@ -1,7 +0,0 @@
-package questao19.agendaTelefonica.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import questao19.agendaTelefonica.model.Telefone;
-
-public interface TelefoneRepository extends JpaRepository<Telefone,Long> {
-}
